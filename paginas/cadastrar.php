3b1a0256e3ebc804673ae6df664425ab4c81b750
MatheusBiazzi <124881548+MatheusBiazzi@users.noreply.github.com>
2024-06-27 20:31:39 -0300

    Update cadastrar.php

--- paginas/cadastrar.php
+++ paginas/cadastrar.php
@@ -1,4 +1,4 @@
-<?php
+<?php       
 if(isset($_POST['submit'])){
 include_once('conexao.php');
 
@@ -6,11 +6,11 @@ $nome =$_POST['nome'];
 $email =$_POST['email'];
 $tel =$_POST['telefone'];
 $data_nasc =$_POST['dt_nasc'];
+$cep = $_POST['cep'];
 $rua = $_POST['rua'];
 $numCasa = $_POST['numero'];
 $bairro = $_POST['bairro'];
 $comple = $_POST['complemento'];
-$cep = $_POST['cep'];
 $senha = $_POST['senha'];
 
 $result = mysqli_query($conn,"INSERT INTO pessoas(nome,
@@ -18,7 +18,7 @@ email, telefone, dt_nasc,cep,rua,numero,bairro,complemento,senha) values('$nome'
 
 echo('cadastrado com sucesso: ');
 }else{
-    echo "tente de novo"
+    echo "tente de novo";
 }
 ?>
 
